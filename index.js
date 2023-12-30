d842793a67fc8a4e073ed7a926dbbcb913f91700
Depasinre Yuan <depasinre@gmail.com>
2023-12-29 19:14:03 -0600

    change the package to esm, and introduce the lowdb and lodash

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let express = require('express');
+import express from 'express';
+import path from 'path';
+// Introduction of lowdb data storage solution
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+import lodash from 'lodash'
 
 const app = express();
+const port = 3000;
 
- 
+// declare the static file folder for css
+app.use(express.static('client'));
+
+// setting the app port
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+})
+
+// When listen the client conection, send the index.html to display
+app.get('/', (req, res)=>{
+   res.sendFile(__dirname + "/" + "client/" +"index.html");
+})
